13a32365c27381ce3b656d9f971416459a895a8f
Patryk Karasiński <patrykkarasinski11@gmail.com>
2021-09-17 16:04:32 +0200

    portfolio

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const btnsCode = [...document.querySelectorAll('.btn-code')];
+const listsCode = [...document.querySelectorAll('.list-code')];
+
+function displayListCode() {
+    console.log(this);
+    listsCode.forEach((listCode, idx) => {
+        if (listCode.dataset.index === this.dataset.index) {
+            listCode.classList.toggle('active');
+            if (listCode.classList.contains('active')) {
+                this.innerHTML = `<p>Kod</p> <i class="fas fa-chevron-up"></i>`;
+            } else {
+                this.innerHTML = `<p>Kod</p> <i class="fas fa-chevron-down"></i>`;
+            }
+        }
+    })
+
+}
+
+btnsCode.forEach((btn, index) => {
+    btn.addEventListener('click', displayListCode);
+})
