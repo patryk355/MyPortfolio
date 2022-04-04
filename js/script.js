3bcf264703754b9a95635a6e530c1bf798f03a57
Patryk <patrykkarasinski123@wp.pl>
2022-04-04 21:23:27 +0200

    change if

--- js/script.js
+++ js/script.js
@@ -2,18 +2,17 @@ const btnsCode = [...document.querySelectorAll('.btn-code')];
 const listsCode = [...document.querySelectorAll('.list-code')];
 
 function displayListCode() {
-    listsCode.forEach((listCode, idx) => {
-        if (listCode.dataset.index === this.dataset.index) {
-            listCode.classList.toggle('active');
-            if (listCode.classList.contains('active')) {
-                this.innerHTML = `<p>Kod</p> <i class="fas fa-chevron-up"></i>`;
-            } else {
-                this.innerHTML = `<p>Kod</p> <i class="fas fa-chevron-down"></i>`;
-            }
-        }
-    })
+  listsCode.forEach((listCode, idx) => {
+    if (listCode.dataset.index === this.dataset.index) {
+      listCode.classList.toggle('active');
+
+      listCode.classList.contains('active')
+        ? (this.innerHTML = `<p>Kod</p> <i class="fas fa-chevron-up"></i>`)
+        : (this.innerHTML = `<p>Kod</p> <i class="fas fa-chevron-down"></i>`);
+    }
+  });
 }
 
 btnsCode.forEach((btn, index) => {
-    btn.addEventListener('click', displayListCode);
-})
+  btn.addEventListener('click', displayListCode);
+});
